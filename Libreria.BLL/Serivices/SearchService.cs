6e9bf24a2b2eef09d9da3d85141c909afffcc12e
Flavio Dellacorte <flavio.dellacorte@alten.it>
2025-07-23 15:52:38 +0200

    Final Genric Query definition

--- Libreria.BLL/Serivices/SearchService.cs
+++ Libreria.BLL/Serivices/SearchService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
@@ -16,81 +17,137 @@ namespace BibliotecaBLL.Serivices
 
         public static IQueryable<T> GenericSearch(IQueryable<T> query, KeyValuePair<string, object> parameter)
         {
-            var property = typeof(T).GetProperty(parameter.Key, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
-            if (property is null) throw new Exception($"Error: Not Found Key {parameter.Key} in {typeof(T).Name} class");
-
-            var parameterExpression = Expression.Parameter(typeof(T), "x");
-            var propertyAccess = Expression.Property(parameterExpression, property);
-
-            Expression filterExpression = null;
-
-            if (property.PropertyType == typeof(string))
+            // OrderBy Section
+            if (parameter.Key.ToLower() == "orderby")
             {
-                var toLowerMeethod = typeof(string).GetMethod("ToLower", Type.EmptyTypes);
-                var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+                // Recover Orderfield from parameter and verify if is Contained as property on the Entity
+                string orderfield = parameter.Value.ToString();
+                var orderProperty = typeof(T).GetProperty(orderfield, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
 
-                var propertyToLower = Expression.Call(propertyAccess, toLowerMeethod);
-                var valueToLower = Expression.Constant(parameter.Value.ToString().ToLower());
+                if (orderProperty is null) return query;
 
-                filterExpression = Expression.Call(propertyToLower, containsMethod, valueToLower);
+                // Preparetion of the Expression
+                var orderParam = Expression.Parameter(typeof(T), "x");
+                var orderByExpression = Expression.Property(orderParam, orderProperty);
+                var delegateType = typeof(Func<,>).MakeGenericType(typeof(T), orderProperty.PropertyType);
+                var orderByLambda = Expression.Lambda(delegateType, orderByExpression, orderParam);
 
-            }
-            else if (IsNumericType(property.PropertyType))
-            {
+                // Finding the OrderBy Method for Queryable
+                var orderByMethod = typeof(Queryable).GetMethods()
+                    .First(m => m.Name == "OrderBy" && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(typeof(T), orderProperty.PropertyType);
 
-                var targetType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
-                object convertedValue;
-
-                if (parameter.Value == null || parameter.Value == DBNull.Value)
-                {
-                    convertedValue = null;
-                }
-                else if (parameter.Value is System.Text.Json.JsonElement jsonElement)
-                {
-                    // Estrazione da JsonElement a seconda del tipo
-                    if (targetType == typeof(int))
-                        convertedValue = jsonElement.GetInt32();
-                    else if (targetType == typeof(long))
-                        convertedValue = jsonElement.GetInt64();
-                    else if (targetType == typeof(string))
-                        convertedValue = jsonElement.GetString();
-                    else if (targetType == typeof(bool))
-                        convertedValue = jsonElement.GetBoolean();
-                    else if (targetType == typeof(double))
-                        convertedValue = jsonElement.GetDouble();
-                    else
-                        throw new InvalidOperationException($"Unsupported JsonElement to {targetType.Name} conversion.");
-                }
-                else if (targetType.IsInstanceOfType(parameter.Value))
-                {
-                    convertedValue = parameter.Value;
-                }
-                else if (parameter.Value is IConvertible)
-                {
-                    convertedValue = Convert.ChangeType(parameter.Value, targetType);
-                }
-                else
-                {
-                    throw new InvalidOperationException($"Parameter value of type {parameter.Value.GetType().Name} cannot be converted to {targetType.Name}");
-                }
-
-                var constant = Expression.Constant(convertedValue, property.PropertyType);
-                filterExpression = Expression.Equal(propertyAccess, constant);
+                // Invoke of OrderBy on query
+                query = (IQueryable<T>)orderByMethod.Invoke(null, new object[] { query, orderByLambda });
             }
             else
             {
-                var targetType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
-                var convertedValue = Convert.ChangeType(parameter.Value, targetType);
-                var constantValue = Expression.Constant(parameter.Value, property.PropertyType);
-                filterExpression = Expression.Equal(propertyAccess, constantValue);
-            }
+                // Equals section
+                var property = typeof(T).GetProperty(parameter.Key, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
+                if (property is null) return query;
 
-            if (!(filterExpression is null))
-            {
-                var lambda = Expression.Lambda<Func<T, bool>>(filterExpression, parameterExpression);
-                query = query.Where(lambda);
-            }
+                var parameterExpression = Expression.Parameter(typeof(T), "x");
+                var propertyAccess = Expression.Property(parameterExpression, property);
 
+                Expression filterExpression = null;
+               
+                // Equal String section
+                if (property.PropertyType == typeof(string))
+                {
+                    // Recover To Lower Method
+                    var toLowerMeethod = typeof(string).GetMethod("ToLower", Type.EmptyTypes);
+                    // Recover Contains Method
+                    var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+                    
+                    var propertyToLower = Expression.Call(propertyAccess, toLowerMeethod);
+                    var valueToLower = Expression.Constant(parameter.Value.ToString().ToLower());
+
+                    // Create expression with ToLower property and Contains method
+                    filterExpression = Expression.Call(propertyToLower, containsMethod, valueToLower);
+
+                }
+                else if (IsNumericType(property.PropertyType)) // Equal Numeric Section
+                {
+
+                    var targetType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                    object convertedValue;
+
+                    if (parameter.Value == null || parameter.Value == DBNull.Value)
+                    {
+                        convertedValue = null;
+                    }
+                    else if (parameter.Value is System.Text.Json.JsonElement jsonElement)
+                    {
+                        // Estraction of the Json Element for  the target type
+                        if (targetType == typeof(int))
+                            convertedValue = jsonElement.GetInt32();
+                        else if (targetType == typeof(long))
+                            convertedValue = jsonElement.GetInt64();
+                        else if (targetType == typeof(string))
+                            convertedValue = jsonElement.GetString();
+                        else if (targetType == typeof(bool))
+                            convertedValue = jsonElement.GetBoolean();
+                        else if (targetType == typeof(double))
+                            convertedValue = jsonElement.GetDouble();
+                        else
+                            throw new InvalidOperationException($"Unsupported JsonElement to {targetType.Name} conversion.");
+                    }
+                    else if (targetType.IsInstanceOfType(parameter.Value))
+                    {
+                        convertedValue = parameter.Value;
+                    }
+                    else if (parameter.Value is IConvertible)
+                    {
+                        convertedValue = Convert.ChangeType(parameter.Value, targetType);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Parameter value of type {parameter.Value.GetType().Name} cannot be converted to {targetType.Name}");
+                    }
+
+                    var constant = Expression.Constant(convertedValue, property.PropertyType);
+                    filterExpression = Expression.Equal(propertyAccess, constant);
+
+                }
+                else if (property.PropertyType == typeof(DateTime) ||  property.PropertyType == typeof(DateOnly) || property.PropertyType == typeof(DateTime?) || property.PropertyType == typeof(DateOnly?))
+                {
+
+                    var targetType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                    object convertedDate = default;
+
+                    if (parameter.Value is null || parameter.Value == DBNull.Value)
+                    {
+                        convertedDate = null;
+                    }
+                    else if (parameter.Value is System.Text.Json.JsonElement jsonElment)
+                    {
+                        if (targetType == typeof(DateTime))
+                            convertedDate = jsonElment.GetDateTime();
+                        else if (targetType == typeof(DateOnly))
+                            convertedDate = DateOnly.FromDateTime(jsonElment.GetDateTime());
+                        else
+                            return query;
+                    }
+                    else if (targetType.IsInstanceOfType(parameter.Value))
+                    {
+                        convertedDate = parameter.Value;
+                    }
+                    var constantDate = Expression.Constant(convertedDate, property.PropertyType);
+                    filterExpression = Expression.Equal(propertyAccess, constantDate);
+                }
+                
+
+                if (!(filterExpression is null))
+                {
+                    // Create lambda Expression with filter selected
+                    var lambda = Expression.Lambda<Func<T, bool>>(filterExpression, parameterExpression);
+                    // Apply the filter method to the query with Where clause
+                    query = query.Where(lambda);
+                }
+
+            }
+           
             return query;
 
         }
